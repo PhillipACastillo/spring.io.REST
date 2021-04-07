276a32400e13dac7d894f699fa5981b45925f8e4
Phillip Castillo <PhillipACastillo@gmail.com>
2021-04-07 10:29:20 -0700

    Create domain entity employee, allow for data persistence via the employee repository, load values into the database with a load database class that employs repository methods

---
+++ src/main/java/com/springframework/payroll/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.springframework.payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
